7e83b1ca38a51ed1bc6267a5fe6260aea7d8a4d9
hayley.mcc@gmail.com <hayley.mcc@gmail.com>
2022-06-20 16:06:07 +0100

    Added pages for About, Homepage and Not Found. Created components for Footer and Navbar. Added routes for all pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// Components
+import UserResults from '../components/users/UserResults';
+
+function Home() {
+	return (
+		<>
+			{/* SEARCH COMPONENT */}
+			<UserResults />
+		</>
+	);
+}
+
+export default Home;
