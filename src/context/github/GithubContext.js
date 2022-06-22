2fc2efeb06b7aabcbd23f0c8e9d652ef512da061
hayley.mcc@gmail.com <hayley.mcc@gmail.com>
2022-06-22 22:18:36 +0100

    Added pages and components for displaying user profile and repos

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -9,6 +9,8 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 export const GithubProvider = ({ children }) => {
 	const initialState = {
 		users: [],
+		user: {},
+		repos: [],
 		loading: false,
 	};
 
@@ -54,6 +56,54 @@ export const GithubProvider = ({ children }) => {
 		});
 	};
 
+	// Get single user
+	const getUser = async (login) => {
+		setLoading();
+
+		const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+			headers: {
+				Authorization: `token ${GITHUB_TOKEN}`,
+			},
+		});
+
+		if (response.status === 404) {
+			window.location = '/notfound';
+		} else {
+			const data = await response.json();
+
+			dispatch({
+				type: 'GET_USER',
+				payload: data,
+			});
+		}
+	};
+
+	// Get user repos
+	const getUserRepos = async (login) => {
+		setLoading();
+
+		const params = new URLSearchParams({
+			sort: 'updated',
+			per_page: 10,
+		});
+
+		const response = await fetch(
+			`${GITHUB_URL}/users/${login}/repos?${params}`,
+			{
+				headers: {
+					Authorization: `token ${GITHUB_TOKEN}`,
+				},
+			}
+		);
+
+		const data = await response.json();
+
+		dispatch({
+			type: 'GET_REPOS',
+			payload: data,
+		});
+	};
+
 	// Clear users from state
 	const clearUsers = () => {
 		dispatch({ type: 'CLEAR_USERS' });
@@ -69,7 +119,11 @@ export const GithubProvider = ({ children }) => {
 			value={{
 				users: state.users,
 				loading: state.loading,
+				user: state.user,
+				repos: state.repos,
 				searchUsers,
+				getUser,
+				getUserRepos,
 				clearUsers,
 			}}>
 			{children}
